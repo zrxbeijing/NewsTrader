d280443130b446fb0555b9d785f9696452b8b73a
zhangrongxin <zrxbeijing@gmail.com>
2021-09-10 22:20:44 +0200

    This is for adding description in the __init__.py file

--- NewsTrader/__init__.py
+++ NewsTrader/__init__.py
@@ -1,5 +1,6 @@
 """
 NewsTrader - a framework of news trading for individual investors
+This package is inspired by many other awesome Python packages
 """
 
 # Shortcuts for key modules or functions
