ae69cac00a72bd00b18513cc02a74d6b17f7d8c3
zhangrongxin <zrxbeijing@gmail.com>
2021-09-11 21:39:02 +0200

    init

---
+++ NewsTrader/newsfeed/__init__.py
@@ -0,0 +1,8 @@
+"""
+Extract news from internet or open-source news database
+1. News from gdelt database (gkg, events)
+2. News from gdelt geg database
+3. News from Common Crawl news dataset
+4. News from internet via scrapy
+"""
+
